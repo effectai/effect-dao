832561a5435db4b635a2a0374f221ec02b60a083
Jesse Eisses <jesse.eisses@gmail.com>
2020-11-16 19:43:00 +0100

    Set environment to mainnet

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,27 +4,27 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
-  // env: {
-  //   eosNodeUrl: 'eos.greymass.com',
-  //   eosChainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
-  //   explorerUrl: 'https://bloks.io',
-  //   tokenContract: 'effecttokens',
-  //   stakingContract: 'efxstakepool',
-  //   daoContract: 'thedaonkylin',
-  //   efxToken: 'EFX',
-  //   nfxToken: 'NFX'
-  // },
-
   env: {
-    eosNodeUrl: 'kylin.eosn.io',
-    eosChainId: '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191',
-    explorerUrl: 'https://kylin.bloks.io',
-    tokenContract: 'tokenonkylin',
-    stakingContract: 'stak3onkylin',
-    daoContract: 'thedaonkylin',
-    efxToken: 'UTL',
-    nfxToken: 'GRN'
+    eosNodeUrl: 'eos.greymass.com',
+    eosChainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
+    explorerUrl: 'https://bloks.io',
+    tokenContract: 'effecttokens',
+    stakingContract: 'efxstakepool',
+    daoContract: 'theeffectdao',
+    efxToken: 'EFX',
+    nfxToken: 'NFX'
   },
+
+  // env: {
+  //   eosNodeUrl: 'kylin.eosn.io',
+  //   eosChainId: '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191',
+  //   explorerUrl: 'https://kylin.bloks.io',
+  //   tokenContract: 'tokenonkylin',
+  //   stakingContract: 'stak3onkylin',
+  //   daoContract: 'thedaonkylin',
+  //   efxToken: 'UTL',
+  //   nfxToken: 'GRN'
+  // },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {

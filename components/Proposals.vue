85a889bf84cdc3e85cc833d475265b9eff0d8e47
David Britt <david@effect.ai>
2021-03-26 15:30:28 +0100

    Make proposals list more mobile friendly

--- components/Proposals.vue
+++ components/Proposals.vue
@@ -3,28 +3,21 @@
     <div v-if="proposals.length > 0">
       <nuxt-link v-for="proposal in proposals" :key="proposal.id" :to="'/proposals/'+proposal.id" class="box has-shadow-outside is-narrow">
         <div class="media">
-          <div class="media-left">
-            <span class="tag" :class="{'is-success': proposal.status == 'ACTIVE', 'is-warning': proposal.status == 'DRAFT', 'is-link': proposal.status == 'PENDING', 'is-dark': proposal.status == 'CLOSED'}">{{ proposal.status }}</span>
-          </div>
           <div class="media-content has-text-weight-light">
-            <b v-if="proposal.title">#{{ proposal.id }}: {{ proposal.title | truncate(45) }}</b>
+            <b v-if="proposal.title">#{{ proposal.id }}: {{ proposal.title | truncate(60) }}</b>
             <b v-else>...</b>
             <div class="has-text-weight-light">
               <small class="mr-1">by <nuxt-link :to="'/account/'+proposal.author">{{ proposal.author }}</nuxt-link></small>
               <small>requesting <span v-for="(pay, index) in proposal.pay" :key="index"><span v-if="index > 0">,</span> {{ parseInt(pay.field_0.quantity) }} EFX</span></small>
             </div>
-          </div>
-          <div class="media-right has-text-right">
-            <div class="is-size-7">
-              <b>{{ categories[proposal.category] }}</b>
-            </div>
-            <div v-if="proposal.status =='ACTIVE'">
-              <span v-for="result in proposal.vote_counts" :key="result.key" class="vote-result">
-                <small><b :class="{'has-text-success': result.key === 1, 'has-text-danger': result.key === 2}">{{ voteTypes.find((vt) => vt.value == result.key).name }}: {{ result.value }}</b></small>
-              </span>
-            </div>
-            <div v-else-if="proposal.cycle" class="is-size-7 has-text-right">
-              Cycle {{ proposal.cycle }}
+            <div>
+              <span class="tag" :class="{'is-success': proposal.status == 'ACTIVE', 'is-warning': proposal.status == 'DRAFT', 'is-link': proposal.status == 'PENDING', 'is-dark': proposal.status == 'CLOSED'}">{{ proposal.status }}</span>
+              <b class="is-size-7">{{ categories[proposal.category] }}</b>
+              <div v-if="proposal.status =='ACTIVE'">
+                <span v-for="result in proposal.vote_counts" :key="result.key" class="vote-result">
+                  <small><b :class="{'has-text-success': result.key === 1, 'has-text-danger': result.key === 2}">{{ voteTypes.find((vt) => vt.value == result.key).name }}: {{ result.value }}</b></small>
+                </span>
+              </div>
             </div>
           </div>
         </div>

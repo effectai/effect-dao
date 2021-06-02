2075806e57a4b66933d94d77786002c2212e7b80
David Britt <david@effect.ai>
2021-06-02 15:14:22 +0200

    remove https prefix and port postfix from JsonRpc instanstiation

--- plugins/eos.js
+++ plugins/eos.js
@@ -1,7 +1,7 @@
 import { Api, JsonRpc } from 'eosjs'
 
 export default (context, inject) => {
-  const rpc = new JsonRpc(`https://${process.env.eosNodeUrl}:443`)
+  const rpc = new JsonRpc(`${process.env.eosNodeUrl}`)
   const api = new Api({ rpc })
 
   inject('eos', api)

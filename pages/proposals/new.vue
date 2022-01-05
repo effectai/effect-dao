2ad23574e9c28a251dc29a4ce10fc3125b0632a5
rochelle8044 <47731818+rochelle8044@users.noreply.github.com>
2022-01-05 07:27:44 -0600

    Updated proposal categories according to P86.

--- pages/proposals/new.vue
+++ pages/proposals/new.vue
@@ -89,10 +89,13 @@
                 <div class="select" style="width: 100%">
                   <select v-model="proposal.category" required style="width: 100%">
                     <option :value="0" disabled>Governance Proposal</option>
-                    <option :value="1">Marketing</option>
-                    <option :value="2">Design</option>
-                    <option :value="3">Technical</option>
-                    <option :value="4">Other</option>
+                    <option :value="1">Sentiment</option>
+                    <option :value="2">Grant Request</option>
+                    <option :value="3">Marketing</option>
+                    <option :value="4">Cycle Budget</option>
+                    <option :value="5">Network Fees</option>
+                    <option :value="6">Smart Contract Updates</option>
+                    <option :value="7">Other</option>
                   </select>
                 </div>
               </div>

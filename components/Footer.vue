8d57c0acd9759c92df29316ca403affd3380ee7e
David Britt <djmbritt@users.noreply.github.com>
2023-03-07 10:38:43 +0000

    Update discord link

--- components/Footer.vue
+++ components/Footer.vue
@@ -8,7 +8,7 @@
     <div class="content has-text-centered">
       <a href="https://github.com/effectai/effect-dao" target="_blank" class="has-text-grey-light"><font-awesome-icon :icon="['fab', 'github']" class="icon is-medium mx-3"/></a>
       <a href="https://t.me/EffectNetworkDAO" target="_blank" class="has-text-grey-light"><font-awesome-icon :icon="['fab', 'telegram']" class="icon is-medium mx-3"/></a>
-      <a href="https://discord.gg/5QxXdmgU" target="_blank" class="has-text-grey-light"><font-awesome-icon :icon="['fab', 'discord']" class="icon is-medium mx-3"/></a>
+      <a href="https://discord.gg/effectnetwork" target="_blank" class="has-text-grey-light"><font-awesome-icon :icon="['fab', 'discord']" class="icon is-medium mx-3"/></a>
       <a href="https://twitter.com/effectaix" target="_blank" class="has-text-grey-light"><font-awesome-icon :icon="['fab', 'twitter']" class="icon is-medium mx-3"/></a>
     </div>
   </footer>
